9e3b596dc71756ce7ffec9907f596d39363eab27
Hutch79 <42042811+Hutch79@users.noreply.github.com>
2024-08-20 14:37:22 +0200

    Fixed dbScript.ps1 errors

--- scripts/dbScript.ps1
+++ scripts/dbScript.ps1
@@ -5,51 +5,51 @@ param(
     [switch]$recreateDatabase
 )
 
-function Display-UsageInstructions {
-    Write-Host ""
-    Write-Host "Usage: .\DatabaseScript.ps1 [-runMigration] [-removeMigrationDirectory] [-recreateDatabase] [-displayHelp]"
-    Write-Host "Parameters:"
-    Write-Host "  -runMigration               Create new migration."
-    Write-Host "  -removeMigrationDirectory   Remove migration directory before executing the script."
-    Write-Host "  -displayHelp                Display this help message."
-    Write-Host ""
+function Show-UsageInstructions {
+    Write-Output ""
+    Write-Output "Usage: .\DatabaseScript.ps1 [-runMigration] [-removeMigrationDirectory] [-recreateDatabase] [-displayHelp]"
+    Write-Output "Parameters:"
+    Write-Output "  -runMigration               Create new migration."
+    Write-Output "  -removeMigrationDirectory   Remove migration directory before executing the script."
+    Write-Output "  -displayHelp                Display this help message."
+    Write-Output ""
 }
 
 if (($PSBoundParameters.Count -eq 0) -or $displayHelp) {
-    Display-UsageInstructions
+    Show-UsageInstructions
     exit
 }
 
 if($removeMigrationDirectory) {
     $directoryPath = "..\Infrastructure\Migrations"
 
-    Write-Host ""
-    Write-Host "==================================================" -ForegroundColor Blue
-    Write-Host "           DELETING MIGRATION FOLDER"
-    Write-Host "==================================================" -ForegroundColor Blue
+    Write-Output ""
+    Write-Output "==================================================" -ForegroundColor Blue
+    Write-Output "           DELETING MIGRATION FOLDER"
+    Write-Output "==================================================" -ForegroundColor Blue
 
     if (Test-Path -Path $directoryPath -PathType Container) {
-        Write-Host ""
-        Write-Host "Deleting directory:"
-        Write-Host "$directoryPath"
+        Write-Output ""
+        Write-Output "Deleting directory:"
+        Write-Output "$directoryPath"
         Remove-Item -Path $directoryPath -Recurse -Force
-        Write-Host ""
-        Write-Host "Directory deleted successfully."
+        Write-Output ""
+        Write-Output "Directory deleted successfully."
     } else {
-        Write-Host ""
-        Write-Host "Directory not found:"
-        Write-Host "$directoryPath"
+        Write-Output ""
+        Write-Output "Directory not found:"
+        Write-Output "$directoryPath"
     }
 }
 
 if($runMigration) {
-	Write-Host ""
-    Write-Host "==================================================" -ForegroundColor Blue
-    Write-Host "               CREATING MIGRATION"
-    Write-Host "==================================================" -ForegroundColor Blue
-    Write-Host ""
+	Write-Output ""
+    Write-Output "==================================================" -ForegroundColor Blue
+    Write-Output "               CREATING MIGRATION"
+    Write-Output "==================================================" -ForegroundColor Blue
+    Write-Output ""
 
-    $MigrationName = Read-Host "Please enter the Migration name"
+    $MigrationName = Read-Output "Please enter the Migration name"
 
     Set-Location -Path "..\Infrastructure"
 
@@ -58,8 +58,8 @@ if($runMigration) {
     Set-Location -Path "..\scripts"
 }
 
-Write-Host ""
-Write-Host "==================================================" -ForegroundColor Blue
-Write-Host "                  COMPLETED"
-Write-Host "==================================================" -ForegroundColor Blue
-Write-Host ""
+Write-Output ""
+Write-Output "==================================================" -ForegroundColor Blue
+Write-Output "                  COMPLETED"
+Write-Output "==================================================" -ForegroundColor Blue
+Write-Output ""
