96fa0ebbbec569f5a6c71e8577ecd1a4747e6d68
Hutch79 <42042811+Hutch79@users.noreply.github.com>
2024-08-20 14:56:20 +0200

    Added comment to dummy test

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -5,6 +5,10 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        // Well, that's my testing .-.
+        // What did you expect, im lazy!
+        // Ok, jokes aside. There will be unit tests which will be (hopefully) better than this.
+        // But currently there are none.
         Assert.True(true);
     }
 }
